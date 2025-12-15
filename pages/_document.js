25dfb0ba56b7c46bde5adfbf6f61ae3a69f74225
MikeWill07 <Mike.Rego@baymetrics.com.br>
2025-12-15 18:13:55 -0300

    add web_id

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="trustech-domain-verification"
-          content="954aca25-6393-49df-b92b-a75b956a9724"
+          content="05e858a3-a964-4679-ac5f-f0f3a38758a8"
         />
         <script src="https://local.functions.local.nhost.run/v1/injector?org_id=8af13671-40b6-4452-b7e0-e0f0c86c77a7"></script>
       </Head>
